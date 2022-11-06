56096c0ddd0be6170770806793abbc8d51488159
Chiamak <46784083+Chiamak@users.noreply.github.com>
2022-11-06 22:14:18 +0100

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,11 @@
+your_name = input("Name:")
+your_age = input("Age:")
+your_dep = input("Department:")
+
+if (your_name == "" or your_age == "" or your_dep == ""):
+    print("Input your complete info please!")
+else:
+    print("Name -> ", your_name)
+    print("Age -> ", your_age) 
+    print("Department -> ", your_dep)
+
